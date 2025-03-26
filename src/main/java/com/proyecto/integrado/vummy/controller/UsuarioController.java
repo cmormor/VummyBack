1ba94f1047d6a691d0fbc86d0d5971bf5a04bd5d
Carmen Morales <carmen.morales@soologic.com>
2025-03-26 14:39:22 +0100

    feat: accept requests from the front

--- src/main/java/com/proyecto/integrado/vummy/controller/UsuarioController.java
+++ src/main/java/com/proyecto/integrado/vummy/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = {"http://127.0.0.1:5173", "https://www.vummyapp.com"})
 @RestController
 @RequestMapping("/api/v1/users")
 public class UsuarioController {
